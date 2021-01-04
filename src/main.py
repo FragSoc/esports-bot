4c4ebcbbd9a55d09e344c4c1525d59c471cce1f6
Ryth-cs <rts512@york.ac.uk>
2021-01-04 20:04:59 +0000

    Transitioned to a class model

--- src/main.py
+++ src/main.py
@@ -1,15 +1,27 @@
 import psycopg2
-conn = psycopg2.connect(
-    host="192.168.1.77",
-    database="esportsbot",
-    user="postgres",
-    password="Pass2020!")
+from psycopg2 import Error
 
-cur = conn.cursor()
-cur.execute('SELECT * FROM voicemaster')
-print(cur.fetchall())
 
-# counter = 0
-# while True:
-#     print(f"LIVE: {counter}")
-#     counter += 1
+class db_connection():
+    def __init__(self):
+        self.conn = psycopg2.connect(host="192.168.1.77",
+                                        database="esportsbot",
+                                        user="postgres",
+                                        password="Pass2020!")
+        self.cur = self.conn.cursor()
+
+    def query(self, query):
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+
+def add_new_vm(vc_id, guild_id, owner_id):
+    db = db_connection()
+    db.query(f'INSERT INTO voicemaster (vc_id, guild_id, owner_id) VALUES ({vc_id}, {guild_id}, {owner_id})')
+    db.close()
+
+#add_new_vm(8, 6, 5)
