abd65f5f593563d62e3142ba7a12f72dc9824fc0
Ryth-cs <rts512@york.ac.uk>
2021-01-14 20:49:39 +0000

    Included new DB call and implemented tweet checker

--- src/cogs/TwitterIntegrationCog.py
+++ src/cogs/TwitterIntegrationCog.py
@@ -1,6 +1,8 @@
 from discord.ext import tasks, commands
 from db_gateway import db_gateway
+from base_functions import get_cleaned_id
 import snscrape.modules.twitter as sntwitter
+import time
 
 class TwitterIntegrationCog(commands.Cog):
 
@@ -23,13 +25,22 @@ class TwitterIntegrationCog(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def addtwitter(self, ctx, twitter_handle=None, announce_channel=None):
-        if twitter_handle is not None:
+        
+        if twitter_handle is not None and announce_channel is not None:
             if (twitter_handle.replace('_', '')).isalnum():
-                await ctx.channel.send(f"{twitter_handle} is valid")
+                twitter_in_db = db_gateway().get('twitter_info', params={'guild_id': ctx.author.guild.id, 'twitter_handle': twitter_handle})
+                if not bool(twitter_in_db):
+                    cleaned_channel_id = get_cleaned_id(announce_channel)
+                    channel_mention = self.bot.get_channel(cleaned_channel_id).mention
+                    previous_tweet_id = self.get_tweets(twitter_handle)[0]['id']
+                    db_gateway().insert('twitter_info', params={'guild_id': ctx.author.guild.id, 'channel_id': cleaned_channel_id, 'twitter_handle': twitter_handle, 'previous_tweet_id': previous_tweet_id})
+                    await ctx.channel.send(f"{twitter_handle} is valid and has been added, their Tweets will be placed in {channel_mention}")
+                else:
+                    await ctx.channel.send(f"{twitter_handle} is already configured to output to {self.bot.get_channel(int(twitter_in_db['channel_id'])).mention}")
             else:
                 await ctx.channel.send("You need to provide a correct Twitter handle")
         else:
-            await ctx.channel.send("You need to provide a Twitter handle")
+            await ctx.channel.send("You need to provide a Twitter handle and a channel")
         
 
     def get_tweets(self, given_username, tweet_number=1):
@@ -45,16 +56,31 @@ class TwitterIntegrationCog(commands.Cog):
 
 
     # https://discordpy.readthedocs.io/en/latest/ext/tasks/
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=50)
     async def tweet_checker(self):
-        print("Looped")
-        for each in self.get_tweets("fragsoc", 1):
-            print(each)
-
+        start_time = time.time()
+        print("** Checking all saved handles **")
+        returned_val = db_gateway().getall('twitter_info')
+        for each in returned_val:
+            single_tweet = self.get_tweets(each['twitter_handle'], 1)
+            if single_tweet[0]['id'] == each['previous_tweet_id']:
+                print(f"{each['twitter_handle']} - Same")
+            else:
+                print(f"{each['twitter_handle']} - Different")
+                await self.bot.get_channel(each['channel_id']).send(f"@{each['twitter_handle']} has just tweeted! Link - {single_tweet[0]['link']}")
+                db_gateway().update('twitter_info', set_params={'previous_tweet_id': int(single_tweet[0]['id'])}, where_params={'guild_id': each['guild_id'], 'twitter_handle': each['twitter_handle']})
+        end_time = time.time()
+        print(f'Checking tweets took: {round(end_time-start_time, 3)}s')
 
     @tweet_checker.before_loop
     async def before_tweet_checker(self):
-        print('waiting...')
+        print('Updating tweets in DB')
+        returned_val = db_gateway().getall('twitter_info')
+        for each in returned_val:
+            single_tweet = self.get_tweets(each['twitter_handle'], 1)
+            db_gateway().update('twitter_info', set_params={'previous_tweet_id': int(single_tweet[0]['id'])}, where_params={'guild_id': each['guild_id'], 'twitter_handle': each['twitter_handle']})
+
+        print('Waiting on bot to become ready before start Twitter cog')
         await self.bot.wait_until_ready()
 
     
