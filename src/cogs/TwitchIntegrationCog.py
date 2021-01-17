c12f459e460aecd7a163381ae97ac600e5005156
Ryth-cs <rts512@york.ac.uk>
2021-01-17 22:07:56 +0000

    DB integration

--- src/cogs/TwitchIntegrationCog.py
+++ src/cogs/TwitchIntegrationCog.py
@@ -1,4 +1,6 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
+from db_gateway import db_gateway
+from base_functions import get_cleaned_id
 import requests
 import time
 import os
@@ -8,16 +10,60 @@ class TwitchIntegrationCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.twitch_handler = TwitchAPIHandler()
+        self.live_checker.start()
+
+    def cog_unload(self):
+        self.live_checker.cancel()
 
     @commands.command()
-    async def test(self, ctx):
-        print("TEST endpoint hit")
-        twitch_handles = ['lcs', 'fragsoc', 'riotgames']
-        print(self.twitch_handler.request_data(twitch_handles))
+    async def addtwitch(self, ctx, twitch_handle=None, announce_channel=None):
+        if twitch_handle is not None and announce_channel is not None:
+            twitch_in_db = db_gateway().get('twitch_info', params={'guild_id': ctx.author.guild.id, 'twitch_handle': twitch_handle})
+            if not bool(twitch_in_db):
+                cleaned_channel_id = get_cleaned_id(announce_channel)
+                channel_mention = self.bot.get_channel(cleaned_channel_id).mention
+                db_gateway().insert('twitch_info', params={'guild_id': ctx.author.guild.id, 'channel_id': cleaned_channel_id, 'twitch_handle': twitch_handle, 'currently_live': False})
+                await ctx.channel.send(f"{twitch_handle} is valid and has been added, their notifications will be placed in {channel_mention}")
+            else:
+                await ctx.channel.send(f"{twitch_handle} is already configured")
+        else:
+            await ctx.channel.send("You need to provide a Twitch handle and a channel")
 
-    
-def setup(bot):
-    bot.add_cog(TwitchIntegrationCog(bot))
+    @commands.command()
+    async def test(self, ctx, twitch_handle=None):
+        start_time = time.time()
+        if twitch_handle is not None:
+            print(self.twitch_handler.request_data([twitch_handle]))
+        else:
+            await ctx.channel.send("You need to provide a Twitch handle")
+        end_time = time.time()
+        print(f'Checking tweets took: {round(end_time-start_time, 3)}s')
+
+    @tasks.loop(seconds=50)
+    async def live_checker(self):
+        start_time = time.time()
+        print("LIVE CHECK!")
+        all_twitch_handles = db_gateway().getall('twitch_info')
+        if all_twitch_handles:
+            twitch_handle_arr = list(map(lambda x: x['twitch_handle'], all_twitch_handles))
+            twitch_handle_arr.append('esl_csgo')
+            twitch_handle_arr.append('zangetsushi')
+            returned_data = self.twitch_handler.request_data(twitch_handle_arr).json()
+            pprint.pprint(returned_data['data'])
+            live_users = list(map(lambda x: x['user_name'].lower(), returned_data['data']))
+            pprint.pprint(live_users)
+            for each in twitch_handle_arr:
+                if each in live_users:
+                    print(f"{each} is LIVE")
+                else:
+                    print(f"{each} is OFFLINE")
+        end_time = time.time()
+        print(f'Checking tweets took: {round(end_time-start_time, 3)}s')
+
+    @live_checker.before_loop
+    async def before_live_checker(self):
+        print('TWITCH: Waiting until bot is ready')
+        await self.bot.wait_until_ready()
 
 
 class TwitchAPIHandler:
@@ -50,13 +96,18 @@ class TwitchAPIHandler:
             oauth_response_json = oauth_response.json()
             oauth_response_json['expires_in'] += time.time()
             self.token = oauth_response_json
+            print("GENERATED NEW TOKEN!")
             return self.token
 
     def request_data(self, twitch_handles):
+        print(f"Current token: {self.token}")
+        if self.token is None or self.token['expires_in'] < time.time():
+            self.generate_new_oauth()
         data_url = 'https://api.twitch.tv/helix/streams?'
         data_url = data_url+"user_login="+("&user_login=".join(twitch_handles))
-        print(data_url)
-        self.generate_new_oauth()
         data_response = requests.get(data_url, headers=self.base_headers(), params=self.params)
-        pprint.pprint(data_response.json())
         return data_response
+
+
+def setup(bot):
+    bot.add_cog(TwitchIntegrationCog(bot))
