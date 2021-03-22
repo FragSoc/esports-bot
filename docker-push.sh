d0a824e650f7a7c0388aa8b9db79b682f813c61b
Laura Demkowicz-Duffy <edward70891@gmail.com>
2021-03-22 20:28:47 +0000

    Add branch-based tagging

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+branch=$(git branch --show)
+docker_tag="fragsoc/esports-bot:${branch}"
+
+docker tag fragsoc/esports-bot ${docker_tag}
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 docker push fragsoc/esports-bot
