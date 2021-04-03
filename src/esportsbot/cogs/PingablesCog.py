42960fefc2545fb596fe40daf44e5a7e32e1d4dd
Trimatix <1jasperlaw@gmail.com>
2021-04-03 02:29:22 +0100

    add basic add-pingable-role and reset-role-ping-cooldown commands

---
+++ src/esportsbot/cogs/PingablesCog.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.context import Context
+from discord.ext.commands.context import Context
+from ..db_gateway import db_gateway
+from ..lib.client import EsportsBot
+
+
+class PingablesCog(commands.Cog):
+    def __init__(self, bot: "EsportsBot"):
+        self.bot: "EsportsBot" = bot
+
+    @commands.command(name="add-pingable-role", usage="add-pingable-role <@role>")
+    async def cmd_add_pingable_role(self, ctx: Context, *, args: str):
+        if len(ctx.message.role_mentions) != 1:
+            await ctx.message.reply("please mention one role")
+        else:
+            role= ctx.message.role_mentions[0]
+            db = db_gateway()
+            roleData = db.get("pingable_roles", {"role_id": role.id})
+            if roleData:
+                await ctx.message.reply("that role is already pingable!")
+            else:
+                db.insert("pingable_roles", {"role_id": role.id, "on_cooldown": False})
+                if not role.mentionable:
+                    await role.edit(mentionable=True, colour=discord.Colour.green(), reason="setting up new pingable role")
+                await ctx.message.reply("pingable role setup complete!")
+
+    
+    @commands.command(name="reset-role-ping-cooldown", usage="reset-role-ping-cooldown <@role>")
+    async def cmd_reset_role_ping_cooldown(self, ctx: Context, *, args: str):
+        if len(ctx.message.role_mentions) != 1:
+            await ctx.message.reply("please mention one role")
+        else:
+            role = ctx.message.role_mentions[0]
+            db = db_gateway()
+            roleData = db.get("pingable_roles", {"role_id": role.id})
+            if not roleData:
+                await ctx.message.reply("that role is not pingable!")
+            elif not roleData[0]["on_cooldown"]:
+                await ctx.message.reply("that role is not on cooldown!")
+            else:
+                db.update("pingable_roles", {"on_cooldown": False}, {"role_id": role.id})
+                if not role.mentionable:
+                    await role.edit(mentionable=True, colour=discord.Colour.green(), reason="manual cooldown reset by user " + str(ctx.author.name) + "#" + str(ctx.author.id))
+                await ctx.message.reply("role has been made pingable again!")
+
+
+
+
+def setup(bot):
+    bot.add_cog(PingablesCog(bot))
