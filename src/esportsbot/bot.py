c76f0be8612bd997242b7e83e715ccb66799a57f
Trimatix <1jasperlaw@gmail.com>
2021-04-04 23:58:46 +0100

    move role ping cooldown awaiting to after commands processing

--- src/esportsbot/bot.py
+++ src/esportsbot/bot.py
@@ -198,22 +198,26 @@ async def on_message(message: discord.Message):
     if message.guild is not None and message.role_mentions:
         db = db_gateway()
         guildInfo = db.get('guild_info', params={'guild_id': message.guild.id})
+        roleUpdateTasks = set()
         if guildInfo:
             roleUpdateTasks = set()
             for role in message.role_mentions:
                 roleData = db.get('pingable_roles', params={'role_id': role.id})
                 if roleData and not roleData[0]["on_cooldown"]:
+                    print("it worked")
                     roleUpdateTasks.add(asyncio.create_task(role.edit(mentionable=False, colour=discord.Colour.darker_grey(), reason="placing pingable role on ping cooldown")))
                     db.update('pingable_roles', where_params={'role_id': role.id},
                                 set_params={'on_cooldown': True, "last_ping": datetime.now().timestamp(),
                                             "ping_count": roleData[0]["ping_count"] + 1, "monthly_ping_count": roleData[0]["monthly_ping_count"] + 1})
                     roleUpdateTasks.add(asyncio.create_task(client.rolePingCooldown(role, guildInfo[0]["role_ping_cooldown_seconds"])))
-            if roleUpdateTasks:
-                await asyncio.wait(roleUpdateTasks)
-                for task in roleUpdateTasks:
-                    if e := task.exception():
-                        traceback.print_exception(type(e), e, e.__traceback__)
-    await client.process_commands(message)
+        await client.process_commands(message)
+        if roleUpdateTasks:
+            await asyncio.wait(roleUpdateTasks)
+            for task in roleUpdateTasks:
+                if e := task.exception():
+                    traceback.print_exception(type(e), e, e.__traceback__)
+    else:
+        await client.process_commands(message)
 
 
 @client.event
