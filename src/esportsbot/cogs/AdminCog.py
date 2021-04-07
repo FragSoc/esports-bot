7d659d4b6ed6023f753dfd73631f348946720ab0
Laura Demkowicz-Duffy <edward70891@gmail.com>
2021-04-07 17:14:11 +0100

    Move admin cog messages to strings file

--- src/esportsbot/cogs/AdminCog.py
+++ src/esportsbot/cogs/AdminCog.py
@@ -1,3 +1,4 @@
+import toml
 from discord.ext import commands
 from ..db_gateway import db_gateway
 from ..base_functions import send_to_log_channel
@@ -6,17 +7,18 @@ from ..base_functions import send_to_log_channel
 class AdminCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.STRINGS = toml.load("../user_strings.toml")["admin"]
 
     @commands.command(aliases=['cls', 'purge', 'delete', 'Cls', 'Purge', 'Delete', 'Clear'])
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=int(amount)+1)
-        await send_to_log_channel(self, ctx.author.guild.id, f"{ctx.author.mention} has cleared {amount} messages from {ctx.channel.mention}")
+        await send_to_log_channel(self, ctx.author.guild.id, self.STRINGS['channel_cleared'].format(author_mention = ctx.author.mention, message_amount=amount))
 
     @commands.command(aliases=['Members'])
     @commands.has_permissions(manage_messages=True)
     async def members(self, ctx):
-        await ctx.channel.send(f"This server has {ctx.guild.member_count} members including me")
+        await ctx.channel.send(self.STRINGS['members'].format(member_count=ctx.guild.member_count)
 
 
 def setup(bot):
