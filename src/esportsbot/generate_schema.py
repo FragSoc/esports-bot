c80c55915c2628ab67a03660883816398dc63148
Trimatix <1jasperlaw@gmail.com>
2021-04-09 22:13:49 +0100

    fix guild_info table definition syntax

--- src/esportsbot/generate_schema.py
+++ src/esportsbot/generate_schema.py
@@ -23,7 +23,7 @@ def generate_schema():
             role_ping_cooldown_seconds bigint NOT NULL,
             pingme_create_threshold int NOT NULL,
             pingme_create_poll_length_seconds bigint NOT NULL,
-            pingme_role_emoji text
+            pingme_role_emoji text,
             shared_role_id bigint
         );
         ALTER TABLE ONLY guild_info
