a6c24a3b5dee535485f76d14af545ede880f9f9c
Fluxticks <benjigarment.appdev@gmail.com>
2021-07-10 14:36:17 +0200

    Implemented load_dict method

--- src/esportsbot/DiscordReactableMenus/EventReactMenu.py
+++ src/esportsbot/DiscordReactableMenus/EventReactMenu.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from discord import Role, TextChannel
 
 from esportsbot.DiscordReactableMenus.ExampleMenus import RoleReactMenu
@@ -9,25 +11,33 @@ class EventReactMenu(RoleReactMenu):
 
     @classmethod
     async def from_dict(cls, bot, data) -> ReactableMenu:
+        kwargs = await cls.load_dict(bot, data)
+
+        menu = EventReactMenu(**kwargs)
+        if menu.enabled:
+            menu.enabled = False
+            await menu.enable_menu(bot)
+
+        return menu
+
+    @classmethod
+    async def load_dict(cls, bot, data) -> Dict:
         kwargs = await super(EventReactMenu, cls).load_dict(bot, data)
 
         shared_role_mentionable = data.get("shared_role")
         shared_role_id = clean_mentioned_role(shared_role_mentionable)
         shared_role = bot.get_guild(data.get("guild_id")).get_role(shared_role_id)
+        kwargs["shared_role"] = shared_role
 
         role_mentionable = list(data.get("options").values())[0].get("descriptor")
         role_id = clean_mentioned_role(role_mentionable)
         event_role = bot.get_guild(data.get("guild_id")).get_role(role_id)
+        kwargs["event_role"] = event_role
 
-        menu = EventReactMenu(event_role=event_role, shared_role=shared_role, **kwargs)
-        if menu.enabled:
-            menu.enabled = False
-            await menu.enable_menu(bot)
+        if kwargs["message"]:
+            kwargs["event_category"] = kwargs["message"].channel.category
 
-        if menu.message:
-            menu.event_category = menu.message.channel.category
-
-        return menu
+        return kwargs
 
     def __str__(self):
         return self.title
@@ -41,7 +51,7 @@ class EventReactMenu(RoleReactMenu):
         super(EventReactMenu, self).__init__(**kwargs)
         self.event_role = event_role
         self.shared_role = shared_role
-        self.event_category = None
+        self.event_category = kwargs.get("event_category", None)
 
     async def finalise_and_send(self, bot, channel: TextChannel):
         await super(EventReactMenu, self).finalise_and_send(bot, channel)
