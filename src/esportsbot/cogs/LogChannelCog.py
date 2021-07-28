a27caa04f53ffc007a84b90b337c724b09be5577
Fluxticks <benjigarment.appdev@gmail.com>
2021-07-28 15:09:34 +0200

    Fixed an issue when the current guild was not in Guild_info table when trying to set/get/remove the logging channel

--- src/esportsbot/cogs/LogChannelCog.py
+++ src/esportsbot/cogs/LogChannelCog.py
@@ -18,36 +18,40 @@ class LogChannelCog(commands.Cog):
     async def setlogchannel(self, ctx, given_channel_id=None):
         cleaned_channel_id = channel_id_from_mention(given_channel_id) if given_channel_id else ctx.channel.id
         guild = DBGatewayActions().get(Guild_info, guild_id=ctx.author.guild.id)
-        log_channel_exists = guild.log_channel_id is not None
-        if log_channel_exists:
-            if guild.log_channel_id != cleaned_channel_id:
-                guild.log_channel_id = cleaned_channel_id
-                DBGatewayActions().update(guild)
-                await ctx.channel.send(self.STRINGS["channel_set"].format(channel_id=cleaned_channel_id))
-                await send_to_log_channel(
-                    self,
-                    ctx.author.guild.id,
-                    self.STRINGS["channel_set_notify_in_channel"].format(author_mention=ctx.author.mention),
-                )
-            else:
-                await ctx.channel.send(self.STRINGS["channel_set_already"])
-        else:
-            guild.log_channel_id = cleaned_channel_id
-            DBGatewayActions().update(guild)
+        if not guild:
+            db_item = Guild_info(guild_id=ctx.guild.id, log_channel_id=cleaned_channel_id)
+            DBGatewayActions().create(db_item)
             await ctx.channel.send(self.STRINGS["channel_set"].format(channel_id=cleaned_channel_id))
             await send_to_log_channel(
                 self,
                 ctx.author.guild.id,
                 self.STRINGS["channel_set_notify_in_channel"].format(author_mention=ctx.author.mention),
             )
+            return
+
+        current_log_channel_id = guild.log_channel_id
+        if current_log_channel_id == cleaned_channel_id:
+            await ctx.channel.send(self.STRINGS["channel_set_already"])
+            return
+
+        guild.log_channel_id = cleaned_channel_id
+        DBGatewayActions().update(guild)
+        await ctx.channel.send(self.STRINGS["channel_set"].format(channel_id=cleaned_channel_id))
+        await send_to_log_channel(
+            self,
+            ctx.author.guild.id,
+            self.STRINGS["channel_set_notify_in_channel"].format(author_mention=ctx.author.mention),
+        )
 
     @commands.command(name="getlogchannel", usage="", help="Gets the server logging channel for bot actions")
     @commands.has_permissions(administrator=True)
     async def getlogchannel(self, ctx):
         guild = DBGatewayActions().get(Guild_info, guild_id=ctx.author.guild.id)
-        log_channel_exists = guild.log_channel_id is not None
+        if not guild:
+            await ctx.channel.send(self.STRINGS["channel_get_notfound"])
+            return
 
-        if log_channel_exists:
+        if guild.log_channel_id:
             await ctx.channel.send(self.STRINGS["channel_get"].format(channel_id=guild.log_channel_id))
         else:
             await ctx.channel.send(self.STRINGS["channel_get_notfound"])
@@ -56,9 +60,11 @@ class LogChannelCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def removelogchannel(self, ctx):
         guild = DBGatewayActions().get(Guild_info, guild_id=ctx.author.guild.id)
-        log_channel_exists = guild.log_channel_id is not None
+        if not guild:
+            await ctx.channel.send(self.STRINGS["channel_get_notfound"])
+            return
 
-        if log_channel_exists:
+        if guild.log_channel_id:
             guild.log_channel_id = None
             DBGatewayActions().update(guild)
             await ctx.channel.send(self.STRINGS["channel_removed"])
