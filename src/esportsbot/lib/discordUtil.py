277df819215843813dba21c666e7f9eeaa2b2371
Fluxticks <30944845+Fluxticks@users.noreply.github.com>
2021-08-19 17:46:18 +0100

    Revert "Implemented posting Twitch Live notifications to Discord Channels  (#140)" (#155)
    This reverts commit 0ddeeb8679a9ccc26ab658db338ef8930cbe3fbc.

--- src/esportsbot/lib/discordUtil.py
+++ src/esportsbot/lib/discordUtil.py
@@ -1,12 +1,10 @@
 import shlex
 
-from discord import Forbidden, RawReactionActionEvent, Message, Member, User, Client, DMChannel, GroupChannel, TextChannel
+from discord import RawReactionActionEvent, Message, Member, User, Client, DMChannel, GroupChannel, TextChannel
 from typing import List, Tuple, Union
 from . import emotes, exceptions
 
 # Link to an empty image, to allow for an author name in embeds without providing an icon.
-from .stringTyping import strIsChannelMention
-
 EMPTY_IMAGE = "https://i.imgur.com/sym17F7.png"
 
 
@@ -82,73 +80,6 @@ async def send_timed_message(channel: TextChannel, *args, timer: int = 15, **kwa
     """
     timed_message = await channel.send(*args, **kwargs)
     await timed_message.delete(delay=timer)
-
-
-def load_discord_hooks(prefix_to_filter, guild_hooks, bot_user_id: int):
-    """
-    Loads the list of Discord Webhooks which are where the Event Notifications are sent to.
-    :param prefix_to_filter: The Prefix to use to filter Webhooks to just the specific cog.
-    :param guild_hooks: The list of lists of Webhooks, where each index is for a different Guild.
-    :param bot_user_id: The Discord user ID of the bot that is running.
-    """
-
-    hooks = {}
-
-    for guild in guild_hooks:
-        # For each guild in the list...
-        for g_hook in guild:
-            # And for each Webhook in the guild...
-            if prefix_to_filter in g_hook.name and g_hook.user.id == bot_user_id:
-                # Only if the Webhook was created for the TwitterCog and by the bot.
-                hooks[g_hook.id] = {"token": g_hook.token, "name": g_hook.name, "guild_id": g_hook.guild_id}
-
-    return hooks
-
-
-async def channel_from_mention(bot, c_id):
-    """
-    Gets an instance of a channel when the channel was mentioned in the message.
-    :param bot: The instance of the bot to access discord with.
-    :param c_id: The mentioned channel.
-    :return: An instance of a channel or None if there is no channel with the given mention.
-    """
-
-    if not strIsChannelMention(c_id):
-        # The string was not a mentioned channel.
-        return None
-
-    # Gets just the ID of the channel.
-    cleaned_id = c_id[2:-1]
-    channel = bot.get_channel(cleaned_id)
-    if channel is None:
-        try:
-            channel = await bot.fetch_channel(cleaned_id)
-        except Forbidden as e:
-            # self.logger.error("Unable to access channel with id %s due to permission errors: %s", cleaned_id, e.text)
-            return None
-    return channel
-
-
-def get_webhook_by_name(current_hooks, name, guild_id, prefix_to_filter):
-    """
-    Gets the information about a Discord Webhook given its name.
-    :param current_hooks: The current known webhooks to search through.
-    :param name: The name of the Webhook.
-    :param guild_id: The ID of the guild where the Webhook is in.
-    :param prefix_to_filter: The prefix used to ensure that the webhook belongs to the cog.
-    :return: A Tuple of hook ID and hook information.
-    """
-
-    # current_hooks = self._twitch_app.hooks
-    if prefix_to_filter not in name:
-        # Only find webhooks created for this cog.
-        name = prefix_to_filter + name
-    for hook in current_hooks:
-        if current_hooks.get(hook).get("name") == name:
-            if current_hooks.get(hook).get("guild_id") == guild_id:
-                return hook, current_hooks.get(hook)
-
-    return None, None
 
 
 def get_attempted_arg(message: str, arg_index: int) -> [str, List]:
