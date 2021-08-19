6c04d20583d14c267ff7eeaf0dfa23e14ea00430
Fluxticks <30944845+Fluxticks@users.noreply.github.com>
2021-08-19 17:30:26 +0100

    Revert "Implemented posting Twitch Live notifications to Discord Channels  (#140)"
    This reverts commit 0ddeeb8679a9ccc26ab658db338ef8930cbe3fbc.

--- src/esportsbot/models.py
+++ src/esportsbot/models.py
@@ -78,11 +78,13 @@ class Voicemaster_slave(base):
 
 class Twitch_info(base):
     __tablename__ = 'twitch_info'
+    id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
     guild_id = Column(BigInteger, nullable=False)
-    channel_id = Column(BigInteger, primary_key=True, nullable=False)
-    hook_id = Column(BigInteger, primary_key=True, nullable=False)
+    channel_id = Column(BigInteger, nullable=False)
     twitch_handle = Column(String, nullable=False)
-    custom_message = Column(String, nullable=True)
+    currently_live = Column(Boolean, nullable=False)
+    custom_message = Column(String, nullable=False)
+    # Will most likely change after Benji switch
 
 
 class Twitter_info(base):
