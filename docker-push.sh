f9bd29f615438167441a9304eaa185d9792ba245
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2021-08-23 23:13:28 +0200

    Rename and add docker push logic

--- docker-push.sh
+++ docker-push.sh
@@ -2,4 +2,4 @@
 
 docker tag fragsoc/esports-bot fragsoc/esports-bot:${DOCKER_TAG}
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
-docker push fragsoc/esports-bot
+docker push fragsoc/esports-bot:${DOCKER_TAG}
