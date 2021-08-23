7f26a345e95b0127997e2e938e5da182de4de698
Laura Demkowicz-Duffy <laura@laurademkowiczduffy.co.uk>
2021-08-24 01:58:28 +0200

    Add esportsbot dir to the sys.path var

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@ import sys
 
 # -- Path setup --------------------------------------------------------------
 
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src/esportsbot'))
+print(sys.path)
 
 # -- Project information -----------------------------------------------------
 
