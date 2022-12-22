e90f52c307f39e79adbb5016ef806845b9e6a965
Fluxticks <benjigarment.appdev@gmail.com>
2022-12-22 16:53:58 +0000

    Added VoiceAdmin Parent and Child models

--- src/database/models.py
+++ src/database/models.py
@@ -4,4 +4,21 @@ from sqlalchemy.ext.declarative import declarative_base
 
 base = declarative_base()
 
-__all__ = ["base"]
+__all__ = ["base", "VoiceAdminParent", "VoiceAdminChild"]
+
+
+class VoiceAdminParent(base):
+    __tablename__ = "voiceadmin_parents"
+    primary_key = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    guild_id = Column(BigInteger, nullable=False)
+    channel_id = Column(BigInteger, nullable=False)
+
+
+class VoiceAdminChild(base):
+    __tablename__ = "voiceadmin_children"
+    primary_key = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    guild_id = Column(BigInteger, nullable=False)
+    channel_id = Column(BigInteger, nullable=False)
+    is_locked = Column(Boolean, nullable=False)
+    is_limited = Column(Boolean, nullable=False)
+    has_custom_name = Column(Boolean, nullable=False)
