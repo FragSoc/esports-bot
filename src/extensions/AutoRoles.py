4b7e28d7c703345421ee1913ce559d15acf4a584
Fluxticks <benjigarment.appdev@gmail.com>
2022-12-25 23:26:16 +0000

    Added listeners to required events

--- src/extensions/AutoRoles.py
+++ src/extensions/AutoRoles.py
@@ -1,17 +1,31 @@
 from discord.ext.commands import Bot, Cog
+from discord import Member
 
 import logging
 from common.io import load_cog_toml
+from client import EsportsBot
 
 COG_STRINGS = load_cog_toml(__name__)
 
 
 class AutoRoles(Cog):
 
-    def __init__(self, bot: Bot):
+    def __init__(self, bot: EsportsBot):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
         self.logger.info(f"{__name__} has been added as a Cog")
+
+    @Cog.listener
+    async def on_member_join(self, member: Member):
+        pass
+
+    @Cog.listener
+    async def on_member_remove(self, member: Member):
+        pass
+
+    @Cog.listener
+    async def on_member_update(self, before: Member, after: Member):
+        pass
 
 
 async def setup(bot: Bot):
