4368f9ca8eca30b2ed35ed6ef8c4781adc2370a8
Fluxticks <benjigarment.appdev@gmail.com>
2023-02-08 11:56:22 +0000

    Applied formatter

--- src/extensions/EventTools.py
+++ src/extensions/EventTools.py
@@ -743,7 +743,10 @@ class EventTools(Cog):
         if event_store is not None:
             DBSession.delete(event_store)
 
-        await interaction.followup.send(content=COG_STRINGS["events_remove_event_success"].format(name=event.name), ephemeral=self.bot.only_ephemeral)
+        await interaction.followup.send(
+            content=COG_STRINGS["events_remove_event_success"].format(name=event.name),
+            ephemeral=self.bot.only_ephemeral
+        )
 
 
 async def setup(bot: Bot):
