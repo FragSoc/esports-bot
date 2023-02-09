94a9164373e8bc51ff121f5135e751744456fabb
Fluxticks <benjigarment.appdev@gmail.com>
2023-02-09 14:21:23 +0000

    Added docstrings to __DBSession functions

--- src/database/gateway.py
+++ src/database/gateway.py
@@ -33,6 +33,17 @@ class __DBSession:
         self.logger.info("Created DB models!")
 
     def list(self, table: Table, **args):
+        """Get multiple values from a query in a given table.
+
+        Args:
+            table (Table): The table to query.
+
+        Raises:
+            Exception: If there was an error while accessing the DB.
+
+        Returns:
+            List: A list of rows from the table that match the paramters given.
+        """
         try:
             return self.session.query(table).filter_by(**args).all()
         except Exception as error:
@@ -43,6 +54,17 @@ class __DBSession:
             raise Exception(f"Error occured when using DB list - {error}")
 
     def get(self, table: Table, **args):
+        """Get a single row from a given Table.
+
+        Args:
+            table (Table): The table to query.
+
+        Raises:
+            Exception: If there was an error while accessing the DB.
+
+        Returns:
+            Any: A row matching the given query. Else None if no rows match the query.
+        """
         try:
             query = self.session.query(table).filter_by(**args).all()
             return query[0] if query != [] else query
@@ -54,6 +76,14 @@ class __DBSession:
             raise Exception(f"Error occured when using DB get - {error}")
 
     def delete(self, record: Any):
+        """Delete a record in a Table.
+
+        Args:
+            record (Any): The record to delete.
+
+        Raises:
+            Exception: If there was an error while accessing the DB.
+        """
         try:
             self.session.delete(record)
             self.session.commit()
@@ -62,6 +92,14 @@ class __DBSession:
             raise Exception(f"Error occured when using DB delete - {error}")
 
     def create(self, record: Any):
+        """Create a new record in a given Table.
+
+        Args:
+            record (Any): The record to insert.
+
+        Raises:
+            Exception: If there was an error while accessing the DB.
+        """
         try:
             self.session.add(record)
             self.session.commit()
@@ -70,6 +108,14 @@ class __DBSession:
             raise Exception(f"Error occured when using DB create - {error}")
 
     def update(self, record: Any):
+        """Update a given record with new data.
+
+        Args:
+            record (Any): The record to update with the changes made to it.
+
+        Raises:
+            Exception: If there was an error while accessing the DB.
+        """
         try:
             self.session.add(record)
             self.session.commit()
