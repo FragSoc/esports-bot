d694d101688615ba8c12eaa9bcb05f94fe87f7f6
Fluxticks <benjigarment.appdev@gmail.com>
2023-03-28 12:29:58 +0200

    Moved user strings to RoleReact.toml

--- src/extensions/default/RoleReact.py
+++ src/extensions/default/RoleReact.py
@@ -27,12 +27,20 @@ async def validate_message_id(interaction: Interaction, message_id: int) -> Unio
     try:
         message = await interaction.channel.fetch_message(message_id)
     except NotFound:
-        await respond_or_followup(f"Unable to find message with ID `{message_id}`", interaction, ephemeral=True)
+        await respond_or_followup(
+            COG_STRINGS["react_warn_message_not_found"].format(message_id=message_id),
+            interaction,
+            ephemeral=True
+        )
         return None
 
     valid_message = DBSession.get(RoleReactMenus, guild_id=message.guild.id, message_id=message.id)
     if not valid_message:
-        await respond_or_followup(f"The given ID of `{message_id}` is not a role react menu", interaction, ephemeral=True)
+        await respond_or_followup(
+            COG_STRINGS["react_warn_invalid_message_found"].format(message_id=message_id),
+            interaction,
+            ephemeral=True
+        )
         return None
 
     return message
@@ -44,7 +52,7 @@ def no_roles_embed(title: str = "Role Menu", embed_color: Color = Color.default(
     if message_id:
         embed.set_footer(text=f"Menu ID: {message_id}")
     else:
-        embed.set_footer(text="Not yet configured...")
+        embed.set_footer(text=COG_STRINGS["react_footer_no_id"])
 
     return embed
 
@@ -92,7 +100,7 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
 
         embed = no_roles_embed(embed_color=embed_color, message_id=message.id)
         await message.edit(embed=embed)
-        await interaction.response.send_message("Menu created!", ephemeral=True)
+        await interaction.response.send_message(COG_STRINGS["react_create_menu_success"], ephemeral=True)
 
     @command(name=COG_STRINGS["react_add_item_name"], description=COG_STRINGS["react_add_item_description"])
     @describe(
@@ -125,7 +133,7 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
         no_roles = not message.components
 
         if no_roles:
-            message_embed.description = "**__Active Roles__**\n"
+            message_embed.description = f"{COG_STRINGS['react_embed_description_title']}\n"
             view = View(timeout=None)
             menu = Select(custom_id=ROLE_REACT_INTERACTION_PREFIX, min_values=0, max_values=1)
         else:
@@ -136,9 +144,19 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
         menu.max_values = len(menu.options) + 1
         menu.add_option(label=f"@{role.name}", value=str(role.id), description=description, emoji=emoji)
         view.add_item(menu)
-        message_embed.description += f"\n{emoji if emoji else ''} {role.mention} - {description if description else ''}"
+        message_embed.description += "\n"
+        message_embed.description += COG_STRINGS["react_role_emoji"].format(emoji=emoji) if emoji else ""
+        message_embed.description += role.mention
+        message_embed.description += COG_STRINGS["react_role_description"].format(
+            description=description
+        ) if description else ""
         await message.edit(embed=message_embed, view=view)
-        await respond_or_followup("Role added!", interaction, ephemeral=True)
+        await respond_or_followup(
+            COG_STRINGS["react_add_item_success"].format(role=role.name,
+                                                         menu_id=message_id),
+            interaction,
+            ephemeral=True
+        )
 
     @command(name=COG_STRINGS["react_remove_item_name"], description=COG_STRINGS["react_remove_item_description"])
     @describe(
@@ -159,7 +177,7 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
         message_embed = message.embeds[0]
         no_roles = not message.components
         if no_roles:
-            await respond_or_followup("No roles to remove!", interaction, ephemeral=True)
+            await respond_or_followup(COG_STRINGS["react_remove_item_warn_no_items"], interaction, ephemeral=True)
             return
 
         view = View.from_message(message, timeout=None)
@@ -171,12 +189,22 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
         else:
             new_description = "**__Active Roles__**\n"
             for option in menu.options:
-                new_description += f"\n{option.emoji if option.emoji else ''} <&@{option.value}> - {option.description if option.description else ''}"
+                new_description += "\n"
+                new_description += COG_STRINGS["react_role_emoji"].format(emoji=option.emoji) if option.emoji else ""
+                new_description += f"<&@{option.value}>"
+                new_description += COG_STRINGS["react_role_description"].format(
+                    description=option.description
+                ) if option.description else ""
             view.add_item(menu)
             message_embed.description = new_description
 
         await message.edit(embed=message_embed, view=view)
-        await respond_or_followup("Successfully removed role!", interaction, ephemeral=True)
+        await respond_or_followup(
+            COG_STRINGS["react_remove_item_success"].format(role_iid=role_id,
+                                                            menu_id=message_id),
+            interaction,
+            ephemeral=True
+        )
 
     @command(name=COG_STRINGS["react_delete_menu_name"], description=COG_STRINGS["react_delete_menu_description"])
     @describe(message_id=COG_STRINGS["react_delete_menu_message_id_describe"])
@@ -191,7 +219,11 @@ class RoleReact(GroupCog, name=COG_STRINGS["react_group_name"]):
         db_item = DBSession.get(RoleReactMenus, guild_id=interaction.guild.id, message_id=message.id)
         DBSession.delete(db_item)
         await message.delete()
-        await respond_or_followup(f"Role menu with id `{message.id}` deleted!", interaction, ephemeral=True)
+        await respond_or_followup(
+            COG_STRINGS["react_delete_menu_success"].format(menu_id=message.id),
+            interaction,
+            ephemeral=True
+        )
 
 
 async def setup(bot: Bot):
