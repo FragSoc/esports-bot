30de08c5469bb9f8facc6ed4283f989ee5a49db9
Fluxticks <benjigarment.appdev@gmail.com>
2023-03-30 13:14:59 +0200

    Basic webhook creation command

--- src/extensions/dynamic/TwitterTracker.py
+++ src/extensions/dynamic/TwitterTracker.py
@@ -1,11 +1,14 @@
 import logging
 
-from discord.app_commands import default_permissions, guild_only
+from discord import Interaction, TextChannel
+from discord.app_commands import default_permissions, guild_only, command, describe, rename
 from discord.ext.commands import Bot, GroupCog
 
+from common.discord import respond_or_followup
 from common.io import load_cog_toml
 
 COG_STRINGS = load_cog_toml(__name__)
+WEBHOOK_PREFIX = __name__
 
 
 @default_permissions(administrator=True)
@@ -16,6 +19,31 @@ class TwitterTracker(GroupCog, name=COG_STRINGS["twitter_group_name"]):
         self.bot = bot
         self.logger = logging.getLogger(__name__)
         self.logger.info(f"{__name__} has been added as a Cog")
+        self.webhooks = {}
+
+    @command(name=COG_STRINGS["twitter_create_webhook_name"], description=COG_STRINGS["twitter_create_webhook_description"])
+    @describe(
+        name=COG_STRINGS["twitter_create_webhook_name_describe"],
+        channel=COG_STRINGS["twitter_create_webhook_channel_describe"]
+    )
+    @rename(
+        name=COG_STRINGS["twitter_create_webhook_name_rename"],
+        channel=COG_STRINGS["twitter_create_webhook_channel_rename"]
+    )
+    async def create_webhook(self, interaction: Interaction, name: str = None, channel: TextChannel = None):
+        await interaction.response.defer(ephemeral=True)
+
+        if not channel:
+            channel = interaction.channel
+
+        if not name:
+            name = f"{WEBHOOK_PREFIX}-{channel.name}"
+
+        webhook = await channel.create_webhook(name=name)
+        if not interaction.guild.id in self.webhooks:
+            self.webhooks[interaction.guild.id] = {}
+        self.webhooks[interaction.guild.id][webhook.id] = webhook
+        await respond_or_followup(f"Created webhook: {webhook.url}", interaction, ephemeral=True, delete_after=None)
 
 
 async def setup(bot: Bot):
