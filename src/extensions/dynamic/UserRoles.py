39d0ee0ac0f883b0740e66a2f4f0415990a77387
Fluxticks <benjigarment.appdev@gmail.com>
2023-08-08 20:23:11 +0200

    Simple vote embed creation

--- src/extensions/dynamic/UserRoles.py
+++ src/extensions/dynamic/UserRoles.py
@@ -1,8 +1,9 @@
 import logging
 from asyncio import create_task
 from asyncio import sleep as async_sleep
+from datetime import datetime
 
-from discord import Role
+from discord import Color, Embed, Interaction, Role
 from discord.app_commands import default_permissions, guild_only
 from discord.ext.commands import Bot, GroupCog
 
@@ -21,6 +22,21 @@ def timeout_role_mention(role: Role, duration: float):
         await role.edit(mentionable=True)
 
     create_task(timeout())
+
+
+def make_vote_embed(role_name: str, vote_duration: float, vote_threshold: int):
+    end = datetime.now().timestamp() + vote_duration
+    end_int = int(end)
+    end_time = f"<t:{end_int}:R>"
+
+    embed = Embed(
+        title=COG_STRINGS["users_vote_menu_title"].format(name=role_name),
+        description=COG_STRINGS["users_vote_menu_description"].format(threshold=vote_threshold,
+                                                                      timestamp=end_time),
+        color=Color.random()
+    )
+
+    return embed
 
 
 @default_permissions(administrator=True)
